f0287d28d351dc90aef5de3cdde67dd06446b837
AvulaRudra <22695A0528@mits.ac.in>
2025-07-13 11:41:06 +0530

    pitchpal update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId = "977686851558-imbj5hp8a9qa9tul1t1qcku44ieplp0u.apps.googleusercontent.com";
+const clientId = "540190260459-2n51r2ta6eki7bsgu87kk4mcp1icdbu4.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={clientId}>
